853c29abf877748a77ddf9ccef9c0de896a0bce8
Ole Morten Halvorsen <olemortenh@gmail.com>
2010-04-11 15:52:13 +0100

    Fixed copyright header.

--- OMHHumanReadableDateTest.m
+++ OMHHumanReadableDateTest.m
@@ -3,7 +3,7 @@
 //  Clyppan
 //
 //  Created by Ole Morten Halvorsen on 22/08/2009.
-//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//  Copyright 2009 omh.cc. All rights reserved.
 //
 
 #import "OMHHumanReadableDateTest.h"
